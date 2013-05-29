29e69c95b299e42855221a6f521e40d1f298d1bc
Ryland Herrick <ryland@bendyworks.com>
2013-05-29 15:36:12 -0500

    Move to embeddable javascript

---
+++ ribbon.js
@@ -0,0 +1,3 @@
+document.write('<link href=\"ribbon.css\" media=\"screen\" rel=\"stylesheet\" />')
+
+document.write('<div class=\"ribbon\"><div class=\"ribbon-content\">Made @ Bendyworks</div></div>')
