adebc08a130967737a74ee14d5bbfcd941b168fb
Ryland Herrick <ryland@bendyworks.com>
2013-05-30 09:48:05 -0500

    Basic idea for customizing ribbon

--- ribbon.js
+++ ribbon.js
@@ -1,3 +1,9 @@
-document.write('<link href=\"ribbon.css\" media=\"screen\" rel=\"stylesheet\" />')
+(function() {
+  var scripts = document.getElementsByTagName('script'),
+      thisScript = scripts[scripts.length - 1],
+      githubLink = thisScript.dataset.github;
 
-document.write('<div class=\"ribbon\"><div class=\"ribbon-content\">Made @ Bendyworks</div></div>')
+  document.write('<link href=\"ribbon.css\" media=\"screen\" rel=\"stylesheet\" />')
+  document.write('<div class=\"ribbon\"><div class=\"ribbon-content\">Made @ Bendyworks')
+  document.write('<a href=\"' + githubLink + '\">github</a></div></div>')
+})()
