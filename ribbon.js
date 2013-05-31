3aded4fb4ac6fdb7d38e04b806b201b8cb25c1ba
Ryland Herrick <ryland@bendyworks.com>
2013-05-30 22:15:19 -0500

    Style to design's mockup
    - Add images

--- ribbon.js
+++ ribbon.js
@@ -1,9 +1,16 @@
 (function() {
-  var scripts = document.getElementsByTagName('script'),
+  var ribbonContent,
+      scripts = document.getElementsByTagName('script'),
       thisScript = scripts[scripts.length - 1],
-      githubLink = thisScript.dataset.github;
+      githubLink = thisScript.getAttribute('data-github'),
+      gravatarLink = thisScript.getAttribute('data-gravatar');
 
-  document.write('<link href=\"ribbon.css\" media=\"screen\" rel=\"stylesheet\" />')
-  document.write('<div class=\"ribbon\"><div class=\"ribbon-content\">Made @ Bendyworks')
-  document.write('<a href=\"' + githubLink + '\">github</a></div></div>')
+  ribbonContent = '<div class=\"ribbon\"><div class=\"ribbon-content\">' +
+      '<a class=\"bw-github\" href=\"' + githubLink + '\" />' +
+      '<a class=\"bw-logo\" href=\"http://bendyworks.com\" />' +
+      '<a class=\"bw-made\" href=\"http://bendyworks.com\" />' +
+      '</div></div>';
+
+  document.write('<link href=\"ribbon.css\" media=\"screen\" rel=\"stylesheet\" />');
+  document.write(ribbonContent);
 })()
