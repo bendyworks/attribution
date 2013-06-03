d59ebc6ca5d7710cc31d7b9ddee3561e9975a9ea
Ryland Herrick <ryland@bendyworks.com>
2013-06-03 15:07:44 -0500

    Customize Pages
    - Add form to generate ribbon

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,32 @@
-console.log('This would be the main JS file.');
+$(function() {
+  $('#generate-ribbon').submit(function(event) {
+    event.preventDefault();
+    var $container = $('#generated'),
+        $generated = $container.find('textarea'),
+        formValues = $(event.target).serializeArray(),
+        $script = $('<script>'),
+        fields = {};
+
+    $.each(formValues, function(_, field) {
+      fields[field.name] = field.value;
+    });
+
+    $script.attr({
+      type: 'text/javascript',
+      src: 'http://d2xzi4fir1vmdf.cloudfront.net/bw-ribbon.js',
+      'data-gh': fields.repo
+    });
+
+    if (fields.gravatar) {
+      var emailHash = md5(fields.gravatar.trim().toLowerCase());
+      $script.attr('data-gr', emailHash);
+
+      if (fields.author) {
+        $script.attr('data-au', fields.author);
+      }
+    }
+
+    $generated.val($('<a>').append($script).html());
+    $container.fadeIn('fast');
+  });
+});
